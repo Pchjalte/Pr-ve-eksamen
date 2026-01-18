bf0879eeb40164efbe3520232a25546ccaa5bbf4
Shuureo <Shuureo.Leo@gmail.com>
2026-01-18 20:53:58 +0100

    death animation and small fixes

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,16 @@
 ﻿using Photon.Pun;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviourPun {
 
     public int maxHealth = 100;
     int currentHealth;
+    public float camHeight = 1.5f;
+    public float fadeSpeed;
+
+    public Image deathFade;
 
     Rigidbody rb;
     Camera cam;
@@ -35,6 +40,7 @@ public class PlayerHealth : MonoBehaviourPun {
         dead = true;
         cam.transform.SetParent(null);
         cam.GetComponent<AudioListener>().enabled = true;
+        StartCoroutine(FadeToBlack());
         StartCoroutine(DeathSequence());
 
         // Restart the whole match
@@ -62,7 +68,7 @@ public class PlayerHealth : MonoBehaviourPun {
 
         // slowly raise camera upward
         Vector3 startPos = cam.transform.position;
-        Vector3 raisedPos = startPos + Vector3.up * 1.5f;
+        Vector3 raisedPos = startPos + Vector3.up * camHeight;
 
         t = 0;
         while (t < 1) {
@@ -83,5 +89,17 @@ public class PlayerHealth : MonoBehaviourPun {
 
         PhotonNetwork.Destroy(gameObject);
         PhotonNetwork.Instantiate("Player", spawn.position, spawn.rotation);
+    }
+
+    IEnumerator FadeToBlack() {
+        float t = 0;
+        Color c = deathFade.color;
+
+        while (t < 1) {
+            t += Time.deltaTime * fadeSpeed;
+            c.a = Mathf.Lerp(0, 1, t);
+            deathFade.color = c;
+            yield return null;
+        }
     }
 }
